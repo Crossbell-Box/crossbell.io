d5cc764cf43edba870f75f17563ce36cde1a8f55
Songkeys <songv587@gmail.com>
2022-08-08 15:27:04 +0800

    chore: bump deps

--- pages/api/link-preview.ts
+++ pages/api/link-preview.ts
@@ -2,30 +2,29 @@ import { getLinkPreview } from "link-preview-js";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const linkPreviewHandler = async (
-  req: NextApiRequest,
-  res: NextApiResponse<Awaited<ReturnType<typeof getLinkPreview>>>,
+	req: NextApiRequest,
+	res: NextApiResponse<Awaited<ReturnType<typeof getLinkPreview>>>
 ) => {
-  const { url } = req.query;
-  console.log("Fetching metadata for: ", url);
-  try {
-    const data = await getLinkPreview(String(url), {
-      timeout: 1000,
-      imagesPropertyType: "og",
-      headers: {
-        "user-agent": "googlebot", // Pretend self as a google bot
-      },
-    });
-    // console.info("Data fetched successfully: ", data);
-    res.json(data);
-  } catch (e) {
-    console.error("Failed to fetch metadata with error", e);
-    res.json({
-      url: String(url),
-      mediaType: "failure",
-      contentType: "unknown",
-      favicons: [],
-    });
-  }
+	const { url } = req.query;
+	try {
+		const data = await getLinkPreview(String(url), {
+			timeout: 1000,
+			imagesPropertyType: "og",
+			headers: {
+				"user-agent": "googlebot", // Pretend self as a google bot
+			},
+		});
+		// console.info("Data fetched successfully: ", data);
+		res.json(data);
+	} catch (e) {
+		console.error("Failed to fetch metadata with error", e);
+		res.json({
+			url: String(url),
+			mediaType: "failure",
+			contentType: "unknown",
+			favicons: [],
+		});
+	}
 };
 
 export default linkPreviewHandler;
