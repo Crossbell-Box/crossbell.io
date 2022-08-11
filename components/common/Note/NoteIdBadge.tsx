674d9184b9b4f92f1b59b83b88977f1c032fd1af
Songkeys <songv587@gmail.com>
2022-08-11 21:29:15 +0800

    fix: open scan tx in new window

--- components/common/Note/NoteIdBadge.tsx
+++ components/common/Note/NoteIdBadge.tsx
@@ -14,6 +14,8 @@ export default function NoteIdBadge({ note }: { note: NoteEntity }) {
 				className="cursor-pointer hover:shadow-sm active:scale-95 transition"
 				component={NextLink}
 				href={composeScanTxHref(note.transactionHash)}
+				target="_blank"
+				rel="noopener noreferrer"
 				variant="gradient"
 				gradient={{ from: theme.colors.brand[6], to: theme.colors.brand[4] }}
 				onClick={(e: any) => e.stopPropagation()}
