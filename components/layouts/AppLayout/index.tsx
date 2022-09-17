35b72aa5d9b800a0d157e8f0ba8904d8530af0a4
Runjuu <hi@runjuu.com>
2022-09-17 19:56:22 +0800

    feat(AppLayout): remove unnecessarry `mainWidth` calculation

--- components/layouts/AppLayout/index.tsx
+++ components/layouts/AppLayout/index.tsx
@@ -2,16 +2,10 @@ import React, { type PropsWithChildren, ReactElement } from "react";
 import { AppShell, Center } from "@mantine/core";
 import Nav from "./Nav";
 import Aside from "./Aside";
-import { useElementSize } from "@mantine/hooks";
 import ScrollToTop from "@/components/common/ScrollToTopButton";
 import { MigrationNotification } from "@/components/biz/migrate";
 
 export default function AppLayout({ children }: PropsWithChildren) {
-	const { ref: rootRef, width: rootWidth } = useElementSize();
-	const { ref: navRef, width: navWidth } = useElementSize();
-	const { ref: asideRef, width: asideWidth } = useElementSize();
-	const mainWidth = rootWidth - navWidth - asideWidth;
-
 	return (
 		<Center>
 			<ScrollToTop />
@@ -19,10 +13,9 @@ export default function AppLayout({ children }: PropsWithChildren) {
 			<MigrationNotification />
 
 			<AppShell
-				ref={rootRef}
 				styles={{
 					root: { maxWidth: "min(100vw, 1400px)" },
-					main: { padding: 0, width: mainWidth },
+					main: { padding: 0, overflow: "hidden" },
 				}}
 				classNames={{
 					root: "w-full",
@@ -32,12 +25,12 @@ export default function AppLayout({ children }: PropsWithChildren) {
 				asideOffsetBreakpoint="sm"
 				fixed={false}
 				navbar={
-					<div ref={navRef}>
+					<div>
 						<Nav />
 					</div>
 				}
 				aside={
-					<div ref={asideRef}>
+					<div>
 						<Aside />
 					</div>
 				}
