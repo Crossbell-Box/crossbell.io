167bd0d02055774d9b49c31938481bef360fd081
Runjuu <hi@runjuu.com>
2022-09-19 15:18:26 +0800

    refactor(aside/NoteOnChainSection): resources path
    https://github.com/Crossbell-Box/crossbell.io/pull/58#discussion_r973874204

--- components/aside/NoteOnChainSection/components/logos.tsx
+++ components/aside/NoteOnChainSection/components/logos.tsx
@@ -2,12 +2,15 @@ import { Text } from "@mantine/core";
 import Image from "next/image";
 import React from "react";
 
+import crossbellLogoUrl from "@/public/images/logo.svg";
+import ipfsLogoUrl from "@/public/logos/ipfs.png";
+
 export const CrossbellLogo = () => (
-	<Image width={19} height={19} src="/images/logo.svg" alt="Crossbell Logo" />
+	<Image width={19} height={19} src={crossbellLogoUrl} alt="Crossbell Logo" />
 );
 
 export const IPFSLogo = () => (
-	<Image width={24} height={24} src="/logos/ipfs.png" alt="Ipfs Logo" />
+	<Image width={24} height={24} src={ipfsLogoUrl} alt="Ipfs Logo" />
 );
 
 export const SourceLogo = () => (
