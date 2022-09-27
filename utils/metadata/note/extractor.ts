59f38fe809ea41bc50c0073b85bf06a21d41bf36
Songkeys <songv587@gmail.com>
2022-09-27 19:23:17 +0800

    fix(markdown): strip frontmatter

--- utils/metadata/note/extractor.ts
+++ utils/metadata/note/extractor.ts
@@ -1,7 +1,5 @@
 import { NoteMetadata } from "crossbell.js";
 import { getValidAttachments } from "./attachment";
-// @ts-ignore
-import removeMd from "remove-markdown";
 
 export function extractCoverImageFromNote(
 	note?: NoteMetadata | null
@@ -71,7 +69,7 @@ export function extractPlainTextFromNote(
 		content = note.title + "\n" + content;
 	}
 
-	let text = removeMd(note.content, { gfm: true, useImgAltText: false });
+	let text = removeMd(note.content, { gfm: true, stripFrontMatter: true });
 
 	if (length) {
 		text = text.slice(0, length);
@@ -82,4 +80,90 @@ export function extractPlainTextFromNote(
 	}
 
 	return text;
+}
+
+// copied from https://github.com/tommoor/remove-markdown/blob/main/index.js
+// and added support for striping front matter
+// may need to extract it to a separate package
+function removeMd(
+	md: string = "",
+	{
+		listUnicodeChar = false,
+		stripListHeaders = false,
+		stripHTML = false,
+		stripFrontMatter = false,
+		gfm = false,
+		keepImgAltText = false,
+	}: {
+		listUnicodeChar?: boolean;
+		stripListHeaders?: boolean;
+		stripHTML?: boolean;
+		stripFrontMatter?: boolean;
+		gfm?: boolean;
+		keepImgAltText?: boolean;
+	} = {}
+) {
+	let output = md;
+
+	try {
+		if (stripFrontMatter) {
+			output = output.replace(/---\s*([\s\S]*?)\s*---/, "");
+		}
+
+		// Remove horizontal rules (stripListHeaders conflict with this rule, which is why it has been moved to the top)
+		output = output.replace(/^(-\s*?|\*\s*?|_\s*?){3,}\s*$/gm, "");
+
+		if (stripListHeaders) {
+			if (listUnicodeChar)
+				output = output.replace(
+					/^([\s\t]*)([\*\-\+]|\d+\.)\s+/gm,
+					listUnicodeChar + " $1"
+				);
+			else output = output.replace(/^([\s\t]*)([\*\-\+]|\d+\.)\s+/gm, "$1");
+		}
+		if (gfm) {
+			output = output
+				// Header
+				.replace(/\n={2,}/g, "\n")
+				// Fenced codeblocks
+				.replace(/~{3}.*\n/g, "")
+				// Strikethrough
+				.replace(/~~/g, "")
+				// Fenced codeblocks
+				.replace(/`{3}.*\n/g, "");
+		}
+		if (stripHTML) {
+			output = output.replace(/<[^>]*>/g, "");
+		}
+
+		output = output
+			// Remove setext-style headers
+			.replace(/^[=\-]{2,}\s*$/g, "")
+			// Remove footnotes?
+			.replace(/\[\^.+?\](\: .*?$)?/g, "")
+			.replace(/\s{0,2}\[.*?\]: .*?$/g, "")
+			// Remove images
+			.replace(/\!\[(.*?)\][\[\(].*?[\]\)]/g, keepImgAltText ? "$1" : "")
+			// Remove inline links
+			.replace(/\[(.*?)\][\[\(].*?[\]\)]/g, "$1")
+			// Remove blockquotes
+			.replace(/^\s{0,3}>\s?/g, "")
+			// Remove reference-style links?
+			.replace(/^\s{1,2}\[(.*?)\]: (\S+)( ".*?")?\s*$/g, "")
+			// Remove atx-style headers
+			.replace(/^(\n)?\s{0,}#{1,6}\s+| {0,}(\n)?\s{0,}#{0,}$/gm, "$1$2")
+			// Remove emphasis (repeat the line to remove double emphasis)
+			.replace(/([\*_]{1,3})(\S.*?\S{0,1})\1/g, "$2")
+			.replace(/([\*_]{1,3})(\S.*?\S{0,1})\1/g, "$2")
+			// Remove code blocks
+			.replace(/(`{3,})(.*?)\1/gm, "$2")
+			// Remove inline code
+			.replace(/`(.+?)`/g, "$1")
+			// Replace two or more newlines with exactly two? Not entirely sure this belongs here...
+			.replace(/\n{2,}/g, "\n\n");
+	} catch (e) {
+		console.error(e);
+		return md;
+	}
+	return output;
 }
