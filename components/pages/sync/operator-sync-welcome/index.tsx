e1796053aeb351e90c9c29835b478b3edc5142c8
Runjuu <hi@runjuu.com>
2022-10-20 19:52:02 -0700

    feat(sync/operator-sync-welcome): avoid unnecessary `setOperator`

--- components/pages/sync/operator-sync-welcome/index.tsx
+++ components/pages/sync/operator-sync-welcome/index.tsx
@@ -2,13 +2,17 @@ import { showNotification } from "@mantine/notifications";
 import { useConnectModal } from "@rainbow-me/rainbowkit";
 import React from "react";
 
-import { useCurrentCharacter } from "@/utils/apis/indexer";
+import {
+	useCharacterOperator,
+	useCurrentCharacter,
+} from "@/utils/apis/indexer";
 import {
 	OPERATOR_ADDRESS,
 	useActivateCharacter,
 } from "@/utils/apis/operator-sync";
 import { useLoginChecker } from "@/utils/wallet/hooks";
 import { useSetCharacterOperator } from "@/utils/apis/contract";
+import { isAddressEqual, NIL_ADDRESS } from "@/utils/ethers";
 
 import { SNSIcons } from "./sns-icons";
 import { Scenes } from "./scenes";
@@ -18,23 +22,27 @@ export default function OperatorSyncWelcome() {
 	const { validate } = useLoginChecker();
 	const activate = useActivateCharacter(character?.characterId);
 	const setOperator = useSetCharacterOperator(OPERATOR_ADDRESS);
+	const { data: operator } = useCharacterOperator(character?.characterId);
 	const { openConnectModal } = useConnectModal();
 
 	const handleStart = React.useCallback(async () => {
 		if (character?.characterId && validate()) {
 			await activate.mutateAsync();
-			await setOperator.mutateAsync(character.characterId, {
-				onSuccess: () => {
-					showNotification({
-						message: "Successfully authorized the operator",
-						color: "green",
-					});
-				},
-			});
+
+			if (!operator || isAddressEqual(operator, NIL_ADDRESS)) {
+				await setOperator.mutateAsync(character.characterId, {
+					onSuccess: () => {
+						showNotification({
+							message: "Successfully authorized the operator",
+							color: "green",
+						});
+					},
+				});
+			}
 		} else {
 			openConnectModal?.();
 		}
-	}, [validate, character, activate, setOperator, openConnectModal]);
+	}, [operator, validate, character, activate, setOperator, openConnectModal]);
 
 	return (
 		<div className="relative z-0 pt-70px pb-100px">
