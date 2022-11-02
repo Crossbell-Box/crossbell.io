dec0cde4d9b44c7c77b1650005af4048c5f3c533
Runjuu <hi@runjuu.com>
2022-11-02 00:14:31 -0700

    fix(achievement/AchievementSection): illustration style issues

--- components/aside/AchievementSection/illustration.tsx
+++ components/aside/AchievementSection/illustration.tsx
@@ -18,20 +18,19 @@ export function Illustration({ url, buttonText }: IllustrationProps) {
 		<BaseSection title={null}>
 			<div className="relative z-0">
 				<div className="pointer-events-none leading-0">
-					<Image src={url} alt={`${buttonText} Illustration`} />
+					<Image
+						src={url}
+						className="max-w-full h-auto"
+						alt={`${buttonText} Illustration`}
+					/>
 				</div>
 
 				<div className="absolute left-0 top-0 w-full h-full flex flex-col items-center">
 					<Text className="text-[#687792] text-16px leading-24px font-500 tracking-0.15px mb-8px self-stretch p-16px">
 						Achievement
 					</Text>
-					<Link href={`/@${handle}/achievement`} passHref>
-						<Button
-							className="mt-auto mb-16px px-24px"
-							color="purple"
-							radius="xl"
-							size="md"
-						>
+					<Link href={`/@${handle}/achievement`} className="mt-auto mb-16px">
+						<Button className="px-24px" color="purple" radius="xl" size="md">
 							{buttonText}
 						</Button>
 					</Link>
