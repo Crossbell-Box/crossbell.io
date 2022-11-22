562c833c3bc6e03c532af2069eff58f87d56e40e
Runjuu <hi@runjuu.com>
2022-11-22 12:47:02 -0800

    feat(ConnectButton): use `DisconnectModal` to replace custom modal

--- components/common/ConnectButton/_WalletButtonWithMenu.tsx
+++ components/common/ConnectButton/_WalletButtonWithMenu.tsx
@@ -6,7 +6,6 @@ import {
 	ExportCrossbellDataHref,
 	WalletCharacterManageHref,
 } from "@/utils/url";
-import Modal from "@/components/common/Modal";
 
 import { ConnectButtonProps } from "./index";
 import WalletDisplayButton from "./_WalletDisplayButton";
@@ -14,7 +13,7 @@ import AccountList from "./_AccountList";
 import MenuItem from "./_MenuItem";
 
 import {
-	useDisconnectAccount,
+	useDisconnectModal,
 	useAccountBalance,
 	GeneralAccount,
 } from "@/components/connectkit";
@@ -29,76 +28,62 @@ export default function WalletButtonWithMenu({
 	account,
 }: WalletButtonWithMenuProps) {
 	const [menuOpened, menuHandlers] = useDisclosure(false);
-	const [disconnOpened, disconnHandlers] = useDisclosure(false);
 	const { balance, isLoading: isLoadingBalance } = useAccountBalance();
-	const disconnectAccount = useDisconnectAccount();
+	const disconnectModal = useDisconnectModal();
 
 	return (
-		<>
-			<Modal
-				opened={disconnOpened}
-				onClose={() => disconnHandlers.close()}
-				title="Disconnect Wallet?"
-				onConfirm={disconnectAccount}
-				confirmText="Disconnect"
-				confirmType="danger"
-			>
-				<Text>You can always log back in at any time.</Text>
-			</Modal>
+		<Menu
+			opened={menuOpened}
+			onOpen={() => menuHandlers.open()}
+			onClose={() => menuHandlers.close()}
+			position="bottom-start"
+			radius="md"
+			transition="scale-y"
+			width="target"
+		>
+			<Menu.Target>
+				<WalletDisplayButton
+					className="w-full"
+					menuOpened={menuOpened}
+					mode={mode}
+					account={account}
+				/>
+			</Menu.Target>
 
-			<Menu
-				opened={menuOpened}
-				onOpen={() => menuHandlers.open()}
-				onClose={() => menuHandlers.close()}
-				position="bottom-start"
-				radius="md"
-				transition="scale-y"
-				width="target"
-			>
-				<Menu.Target>
-					<WalletDisplayButton
-						className="w-full"
-						menuOpened={menuOpened}
-						mode={mode}
-						account={account}
-					/>
-				</Menu.Target>
+			<Menu.Dropdown className="w-full">
+				<Menu.Label>$CSB Balance</Menu.Label>
+				<MenuItem>
+					<div className="flex items-center">
+						<Text className="i-csb:logo text-lg" color="black" />
+						<Space w={5} />
+						<Text className="font-600">
+							{isLoadingBalance ? "..." : balance}
+						</Text>
+					</div>
+				</MenuItem>
 
-				<Menu.Dropdown className="w-full">
-					<Menu.Label>$CSB Balance</Menu.Label>
-					<MenuItem>
-						<div className="flex items-center">
-							<Text className="i-csb:logo text-lg" color="black" />
-							<Space w={5} />
-							<Text className="font-600">
-								{isLoadingBalance ? "..." : balance}
-							</Text>
-						</div>
+				<Menu.Divider />
+
+				<Menu.Label>Characters</Menu.Label>
+				<AccountList />
+
+				<Menu.Divider />
+
+				{account.type === "wallet" && (
+					<MenuItem component={Link} href={WalletCharacterManageHref}>
+						Manage Characters
 					</MenuItem>
+				)}
 
-					<Menu.Divider />
-
-					<Menu.Label>Characters</Menu.Label>
-					<AccountList />
-
-					<Menu.Divider />
-
-					{account.type === "wallet" && (
-						<MenuItem component={Link} href={WalletCharacterManageHref}>
-							Manage Characters
-						</MenuItem>
-					)}
-
-					<MenuItem
-						component={Link}
-						href={ExportCrossbellDataHref}
-						target="_blank"
-					>
-						Export Your Data
-					</MenuItem>
-					<MenuItem onClick={() => disconnHandlers.open()}>Disconnect</MenuItem>
-				</Menu.Dropdown>
-			</Menu>
-		</>
+				<MenuItem
+					component={Link}
+					href={ExportCrossbellDataHref}
+					target="_blank"
+				>
+					Export Your Data
+				</MenuItem>
+				<MenuItem onClick={disconnectModal.show}>Disconnect</MenuItem>
+			</Menu.Dropdown>
+		</Menu>
 	);
 }
