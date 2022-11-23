d498383334db1bae708a7f78d77ab7a8ce7f5e07
Runjuu <hi@runjuu.com>
2022-11-22 16:32:07 -0800

    feat(connectkit/modal-header): remove button outline

--- components/connectkit/components/modal-header/index.tsx
+++ components/connectkit/components/modal-header/index.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import classNames from "classnames";
+
+import styles from "./index.module.css";
 
 export type ModalHeaderProps = {
 	title: React.ReactNode;
@@ -8,7 +11,10 @@ export type ModalHeaderProps = {
 
 export function ModalHeader({ title, leftNode, rightNode }: ModalHeaderProps) {
 	return (
-		<div data-animation="fade-in" className="pb-10px px-21px pt-23px">
+		<div
+			data-animation="fade-in"
+			className={classNames("pb-10px px-21px pt-23px", styles.container)}
+		>
 			<div className="flex items-center justify-between gap-1 relative">
 				<div>{leftNode}</div>
 				<div className="mx-auto text-16px font-500 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
