e1549d340561ac308e6d82d96aa8f51fb128612e
Runjuu <hi@runjuu.com>
2022-12-07 11:48:52 -0800

    dev: move `unocss.config.ts` into project’s `utils` folder

---
+++ utils/unocss/unocss.config.ts
@@ -0,0 +1,68 @@
+import path from "path";
+import {
+	defineConfig,
+	presetAttributify,
+	presetIcons,
+	presetUno,
+} from "unocss";
+
+import { FileSystemIconLoader } from "@iconify/utils/lib/loader/node-loaders";
+
+export default defineConfig({
+	presets: [
+		presetAttributify(),
+		presetIcons({
+			collections: {
+				csb: FileSystemIconLoader(
+					path.join(__dirname, "./icons"),
+					(svg) => svg
+				),
+				// Usage example: <Text className="i-csb:back" />
+			},
+		}),
+		presetUno(),
+	],
+	rules: [
+		["font-roboto", { "font-family": `"Roboto", sans-serif` }],
+		["font-deca", { "font-family": `"Lexend Deca", sans-serif` }],
+	],
+	shortcuts: {
+		"bg-hover": "hover:bg-gray/5 active:bg-gray/10 transition-background-color",
+	},
+	theme: {
+		colors: {
+			brand: {
+				primary: "#FFCF55", // bg-brand-primary
+			},
+			dimmed: "#868e96",
+			red: {
+				light: "#E7AAA1",
+				primary: "#E65040",
+			},
+			yellow: {
+				light: "#FAE3A6",
+				primary: "#FFC53D",
+			},
+			blue: {
+				light: "#B1C3F7",
+				primary: "#5B89F7",
+			},
+			green: {
+				light: "#C0EACB",
+				primary: "#6AD991",
+			},
+			purple: {
+				light: "#C8C3F4",
+				primary: "#9688F2",
+			},
+		},
+		breakpoints: {
+			// https://mantine.dev/styles/responsive/#configure-breakpoints
+			xs: "576px",
+			sm: "768px",
+			md: "992px",
+			lg: "1200px",
+			xl: "1440px",
+		},
+	},
+});
