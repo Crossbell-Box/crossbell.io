8fe1b67cd5320a9abe8b6c76865a4c5926dfe1e1
Runjuu <hi@runjuu.com>
2022-12-07 23:16:46 -0800

    feat(sites): init x-sync

---
+++ sites/x-sync/next.config.js
@@ -0,0 +1,17 @@
+const { default: UnoCSS } = require("@unocss/webpack");
+const { withIpfsGateway } = require("@crossbell/ipfs-gateway-next");
+const {
+	withLocalPackages,
+} = require("../../scripts/nextjs/with-local-packages");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+	webpack: (config) => {
+		config.plugins.push(UnoCSS());
+		config.cache = false;
+		return config;
+	},
+};
+
+module.exports = withIpfsGateway(withLocalPackages(nextConfig));
