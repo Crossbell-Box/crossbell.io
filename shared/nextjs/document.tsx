2e35b3865e5d69ca0837181dcab9618bc4971797
Runjuu <hi@runjuu.com>
2022-12-08 16:08:30 -0800

    refactor: move `_document` into `shared` folder

---
+++ shared/nextjs/document.tsx
@@ -0,0 +1,71 @@
+// https://mantine.dev/theming/next/
+import Document, {
+	DocumentContext,
+	Head,
+	Html,
+	Main,
+	NextScript,
+} from "next/document";
+import { createStylesServer } from "@mantine/next";
+import React from "react";
+import { emotionCache } from "~/shared/providers/theme-provider";
+
+const stylesServer = createStylesServer(emotionCache);
+
+export default class _Document extends Document {
+	static async getInitialProps(ctx: DocumentContext) {
+		// https://github.com/mui/material-ui/blob/master/examples/nextjs-with-typescript/pages/_document.tsx
+
+		const originalRenderPage = ctx.renderPage;
+
+		ctx.renderPage = () =>
+			originalRenderPage({
+				enhanceApp: (App: any) =>
+					function EnhanceApp(props) {
+						return <App emotionCache={emotionCache} {...props} />;
+					},
+			});
+
+		const initialProps = await Document.getInitialProps(ctx);
+		// This is important. It prevents Emotion to render invalid HTML.
+		// See https://github.com/mui/material-ui/issues/26561#issuecomment-855286153
+		const emotionStyles = stylesServer.extractCriticalToChunks(
+			initialProps.html
+		);
+		const emotionStyleTags = emotionStyles.styles.map((style) => (
+			<style
+				data-emotion={`${style.key} ${style.ids.join(" ")}`}
+				key={style.key}
+				// eslint-disable-next-line react/no-danger
+				dangerouslySetInnerHTML={{ __html: style.css }}
+			/>
+		));
+
+		return {
+			...initialProps,
+			emotionStyleTags,
+		};
+	}
+
+	render() {
+		return (
+			<Html>
+				<Head>
+					{/* https://nextjs.org/docs/messages/google-font-display */}
+					<link rel="preconnect" href="https://fonts.bunny.net" />
+					<link
+						href="https://fonts.bunny.net/css?family=lexend-deca:100,200,300,400,500,600,700,800,900|roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i"
+						rel="stylesheet"
+					/>
+					<meta name="emotion-insertion-point" content="" />
+					{(this.props as any).emotionStyleTags}
+				</Head>
+
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
