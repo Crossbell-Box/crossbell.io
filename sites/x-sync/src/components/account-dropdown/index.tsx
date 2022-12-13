3eb8b4a82810feb1ffdd05cb8c5a56655c178028
Runjuu <hi@runjuu.com>
2022-12-13 15:24:27 -0800

    feat(x-sync): add account-dropdown to sidebar

--- sites/x-sync/src/components/account-dropdown/index.tsx
+++ sites/x-sync/src/components/account-dropdown/index.tsx
@@ -6,13 +6,19 @@ import { AccountList } from "~/shared/components/account-list";
 
 export type AccountDropdownProps = {
 	children: React.ReactNode;
+	width?: number;
+	offset?: number;
 };
 
-export function AccountDropdown({ children }: AccountDropdownProps) {
+export function AccountDropdown({
+	children,
+	width = 200,
+	offset,
+}: AccountDropdownProps) {
 	const disconnectModal = useDisconnectModal();
 
 	return (
-		<Menu shadow="md" width={200} radius={12}>
+		<Menu shadow="md" width={width} radius={12} offset={offset}>
 			<Menu.Target>{children}</Menu.Target>
 
 			<Menu.Dropdown className="border-[#E1E8F7]">
--- sites/x-sync/src/components/sidebar/index.tsx
+++ sites/x-sync/src/components/sidebar/index.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import Link from "next/link";
-
 import { Text } from "@mantine/core";
 
-import { CrossbellLogo } from "@crossbell/ui";
+import { useAccountCharacter } from "@crossbell/connect-kit";
+import { extractCharacterName } from "@crossbell/util-metadata";
+
 import { Logo } from "@/components/logo";
+import { AccountDropdown } from "@/components/account-dropdown";
+
+import { Avatar } from "~/shared/components/avatar";
 
 export function Sidebar() {
+	const character = useAccountCharacter();
+
 	return (
-		<div className="w-280px sticky top-0 px-32px pt-25px pb-16px min-h-100vh flex flex-col items-center">
+		<div className="w-280px sticky top-0 px-24px pt-25px pb-16px min-h-100vh flex flex-col items-center">
 			<Link href="/" className="mb-100px w-full">
 				<Logo />
 			</Link>
@@ -25,9 +31,20 @@ export function Sidebar() {
 				</li>
 			</ul>
 
-			<a href="https://crossbell.io" className="mt-auto" target="_blank">
-				<CrossbellLogo className="w-86px" />
-			</a>
+			<div className="flex-1" />
+
+			<AccountDropdown width={257} offset={12}>
+				<button className="flex items-center justify-center w-full p-10px bg-[#5B89F7] text-[#FFF] rounded-12px gap-[10px] border-none ux-overlay">
+					<Avatar
+						size={24}
+						character={character}
+						className="border-2 border-[#FFF]"
+					/>
+					<Text className="truncate font-14px font-roboto font-400">
+						{extractCharacterName(character)}
+					</Text>
+				</button>
+			</AccountDropdown>
 		</div>
 	);
 }
