480d6f8b7775f7ce1e03c53d6976f428159f73e2
Runjuu <hi@runjuu.com>
2022-12-13 14:38:00 -0800

    refactor: move `getDefaultAvatar` into shared folder

--- shared/components/avatar/index.tsx
+++ shared/components/avatar/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useCharacter } from "@crossbell/indexer";
-import { stringToInteger } from "~/shared/helpers";
 import { ipfsLinkToHttpLink } from "@crossbell/util-ipfs";
 import { extractCharacterAvatar } from "@crossbell/util-metadata";
 import { Avatar as Avatar_, AvatarProps } from "@mantine/core";
 import { CharacterEntity } from "crossbell.js";
 import { PropsWithChildren } from "react";
+
 import CharacterHoverCard from "~/shared/components/character/character-hover-card";
+import { getDefaultAvatar } from "~/shared/avatar";
 
 export function Avatar({
 	characterId,
@@ -58,26 +59,4 @@ export function Avatar({
 			/>
 		</CharacterHoverCard>
 	);
-}
-
-const defaultAvatars = [
-	"/images/avatars/bell-black.jpg",
-	"/images/avatars/bell-blue.jpg",
-	"/images/avatars/bell-green.jpg",
-	"/images/avatars/bell-purple.jpg",
-	"/images/avatars/bell-red.jpg",
-	"/images/avatars/bell-white.jpg",
-	"/images/avatars/bell-yellow.jpg",
-];
-
-function getDefaultAvatar(handle?: string) {
-	if (!handle || (handle.startsWith("0x") && handle.length === 42)) {
-		return "/images/avatar-default.png";
-	}
-
-	const seededRandomIndex = stringToInteger(handle, {
-		min: 0,
-		max: defaultAvatars.length - 1,
-	});
-	return defaultAvatars[seededRandomIndex];
 }
