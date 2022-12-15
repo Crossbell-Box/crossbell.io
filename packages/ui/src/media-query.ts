d3ce548e65b40bd8c3dedd3bcbc166f145194c5a
Runjuu <hi@runjuu.com>
2022-12-14 20:08:04 -0800

    feat(x-sync): adjust `switch-apps-btn` for mobile

--- packages/ui/src/media-query.ts
+++ packages/ui/src/media-query.ts
@@ -1,19 +1,3 @@
-import facepaint from "facepaint";
+import { breakpoints } from "~/scripts/unocss/breakpoints";
 
-import { breakpoints } from "../../../scripts/unocss/breakpoints";
-
-export const breakpoint = {
-	xs: `@media (min-width: ${breakpoints.xs}px)`,
-	sm: `@media (min-width: ${breakpoints.sm}px)`,
-	md: `@media (min-width: ${breakpoints.md}px)`,
-	lg: `@media (min-width: ${breakpoints.lg}px)`,
-	xl: `@media (min-width: ${breakpoints.xl}px)`,
-};
-
-export const mq = facepaint([
-	breakpoint.xs,
-	breakpoint.sm,
-	breakpoint.md,
-	breakpoint.lg,
-	breakpoint.xl,
-]);
+export { breakpoints };
--- sites/x-sync/src/components/switch-apps-btn/index.tsx
+++ sites/x-sync/src/components/switch-apps-btn/index.tsx
@@ -1,64 +1,107 @@
 import React from "react";
-import { Text, Menu } from "@mantine/core";
+import { Text, Menu, Modal, CloseButton } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import classNames from "classnames";
 import { useAccount } from "wagmi";
 
-import { XSyncLogo, XCharLogo, XFeedLogo, XShopLogo } from "@crossbell/ui";
+import {
+	XSyncLogo,
+	XCharLogo,
+	XFeedLogo,
+	XShopLogo,
+	breakpoints,
+} from "@crossbell/ui";
 import config from "~/shared/config";
+import { useMediaQuery } from "@mantine/hooks";
 
 const cls = {
 	app: classNames(
 		"flex flex-col items-center px-2 py-1 rounded-md hover:underline"
 	),
 	logo: classNames("w-36px h-36px"),
-	logoTitle: classNames("font-deca font-600 text-14px"),
+	logoTitle: classNames("font-deca font-600 text-14px truncate"),
 };
 
 export function SwitchAppsBtn() {
 	const { address } = useAccount();
-
-	return (
-		<Menu width={257} radius={28}>
-			<Menu.Target>
-				<button className="flex items-center justify-center gap-[16px] w-full p-12px text-[#000] bg-transparent border-1 border-[#E1E8F7] rounded-12px cursor-pointer ux-overlay">
-					<Text className="i-csb:squares text-16px text-[#B5BDCE]" />
-					<div className="flex items-center gap-[4px]">
-						<XSyncLogo className="w-24px h-24px" />
-						<div className="text-16px font-600">xSync</div>
-					</div>
-				</button>
-			</Menu.Target>
-
-			<Menu.Dropdown className="border-[#E1E8F7]">
-				<div className="py-24px flex flex-col gap-[20px] items-center">
-					<div className="font-500 text-16px">Crossbell Apps</div>
-					<div className="flex items-center justify-around w-full">
-						<a className={cls.app} href={config.xSync.domain} target="_blank">
-							<XSyncLogo className={cls.logo} />
-							<div className={cls.logoTitle}>XSync</div>
-						</a>
-
-						<a className={cls.app} href={config.xChar.domain} target="_blank">
-							<XCharLogo className={cls.logo} />
-							<div className={cls.logoTitle}>XChar</div>
-						</a>
-
-						<a className={cls.app} href={config.xFeed.domain} target="_blank">
-							<XFeedLogo className={cls.logo} />
-							<div className={cls.logoTitle}>XFeed</div>
-						</a>
-
-						<a
-							className={cls.app}
-							href={`${config.xShop.domain}/wallet/${address}`}
-							target="_blank"
-						>
-							<XShopLogo className={cls.logo} />
-							<div className={cls.logoTitle}>XShop</div>
-						</a>
-					</div>
-				</div>
-			</Menu.Dropdown>
-		</Menu>
+	const isSM = useMediaQuery(`(min-width: ${breakpoints.sm}px)`);
+	const [isModalOpened, modal] = useDisclosure(false);
+	const btn = (
+		<button
+			onClick={isSM ? undefined : modal.open}
+			className="flex items-center justify-center gap-[16px] w-full p-12px text-[#000] bg-transparent border-1 border-[#E1E8F7] rounded-12px cursor-pointer ux-overlay"
+		>
+			<Text className="i-csb:squares text-16px text-[#B5BDCE]" />
+			<div className="flex items-center gap-[4px]">
+				<XSyncLogo className="w-24px h-24px" />
+				<div className="text-16px font-600">xSync</div>
+			</div>
+		</button>
 	);
+
+	const apps = (
+		<div className="py-24px flex flex-col gap-[20px] items-center">
+			<CloseButton
+				size={28}
+				className="absolute top-22px right-22px sm:hidden"
+				onClick={modal.close}
+			/>
+			<div className="font-500 text-16px">Crossbell Apps</div>
+			<div className="flex items-center justify-around w-full">
+				<a className={cls.app} href={config.xSync.domain} target="_blank">
+					<XSyncLogo className={cls.logo} />
+					<div className={cls.logoTitle}>xSync</div>
+				</a>
+
+				<a className={cls.app} href={config.xChar.domain} target="_blank">
+					<XCharLogo className={cls.logo} />
+					<div className={cls.logoTitle}>xChar</div>
+				</a>
+
+				<a className={cls.app} href={config.xFeed.domain} target="_blank">
+					<XFeedLogo className={cls.logo} />
+					<div className={cls.logoTitle}>xFeed</div>
+				</a>
+
+				<a
+					className={cls.app}
+					href={`${config.xShop.domain}/wallet/${address}`}
+					target="_blank"
+				>
+					<XShopLogo className={cls.logo} />
+					<div className={cls.logoTitle}>xShop</div>
+				</a>
+			</div>
+		</div>
+	);
+
+	if (isSM) {
+		return (
+			<Menu width={257} radius={28}>
+				<Menu.Target>{btn}</Menu.Target>
+
+				<Menu.Dropdown className="border-[#E1E8F7]">{apps}</Menu.Dropdown>
+			</Menu>
+		);
+	} else {
+		return (
+			<>
+				<Modal
+					centered={true}
+					withCloseButton={false}
+					padding={0}
+					radius={0}
+					classNames={{
+						modal: "rounded-28px overflow-hidden mx-16px",
+						inner: "p-0",
+					}}
+					opened={isModalOpened}
+					onClose={modal.close}
+				>
+					{apps}
+				</Modal>
+				{btn}
+			</>
+		);
+	}
 }
