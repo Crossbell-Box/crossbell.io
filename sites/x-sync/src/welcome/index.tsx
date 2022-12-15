79f260849885683378257b79117fd4202f02c482
Runjuu <hi@runjuu.com>
2022-12-15 00:01:17 -0800

    feat(x-sync): tweak `welcome` styles

--- sites/x-sync/src/welcome/index.tsx
+++ sites/x-sync/src/welcome/index.tsx
@@ -11,7 +11,13 @@ export function OperatorSyncWelcome({ onStart }: OperatorSyncWelcomeProps) {
 	return (
 		<div className="relative z-0 min-h-100vh flex items-center justify-center overflow-hidden">
 			<Scenes onStart={onStart} />
-			<div className="absolute left-1/2 top-0 transform -translate-x-1/2 py-30px px-3vw w-full">
+			<div
+				className="absolute left-1/2 top-0 transform -translate-x-1/2 py-30px px-3vw w-full"
+				style={{
+					background:
+						"linear-gradient(transparent, transparent 50%, #FFF), url('/images/header-texture.svg')",
+				}}
+			>
 				<div className="w-full max-w-900px flex items-center justify-between mx-auto min-h-52px">
 					<Link href="/">
 						<Logo />
