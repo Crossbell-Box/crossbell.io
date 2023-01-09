2ca6d0a61087fb671074ec0a5f6a7e250ef4c0c6
Runjuu <hi@runjuu.com>
2023-01-08 19:43:19 -0800

    feat(connect-kit): loading images from url

--- packages/connect-kit/src/utils/index.ts
+++ packages/connect-kit/src/utils/index.ts
@@ -1,5 +1,6 @@
 export * from "./async-retry";
 export * from "./detect-browser";
+export * from "./use-web-2-url";
 export * from "./is-email";
 export * from "./zustand-context";
 export * from "./zustand-slice";
