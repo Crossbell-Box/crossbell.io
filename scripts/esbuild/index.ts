fedf7aadcbeebd01c53e1fce507dad165703bc9c
Runjuu <hi@runjuu.com>
2023-01-09 20:27:56 -0800

    dev(esbuild): update plugins
    using `esbuild-css-modules-plugin` to replace `cssModulePlugin`

--- scripts/esbuild/index.ts
+++ scripts/esbuild/index.ts
@@ -1,3 +1,4 @@
-import { cssModulePlugin } from "./plugins";
+import { Plugin } from "esbuild";
+import cssModulesPlugin from "esbuild-css-modules-plugin";
 
-export const plugins = [cssModulePlugin];
+export const plugins: Plugin[] = [cssModulesPlugin()];
