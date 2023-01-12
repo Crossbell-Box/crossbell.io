89e49d650d223845a4e53a22a6c3fdd02d3b2a36
Runjuu <hi@runjuu.com>
2023-01-11 23:34:41 -0800

    feat(connect-kit): `useMintNote` add extra options

--- packages/connect-kit/src/hooks/use-mint-note.ts
+++ packages/connect-kit/src/hooks/use-mint-note.ts
@@ -1,27 +1,46 @@
 import { useContract } from "@crossbell/contract";
 import { showNotification } from "@mantine/notifications";
-import { useQueryClient, useMutation } from "@tanstack/react-query";
+import {
+	useQueryClient,
+	useMutation,
+	UseMutationOptions,
+} from "@tanstack/react-query";
 import {
 	SCOPE_KEY_MINTED_NOTE_OF_ADDRESS,
 	SCOPE_KEY_MINTED_NOTE_OF_NOTE,
 	SCOPE_KEY_NOTE_STATUS,
 } from "@crossbell/indexer";
+import { useAccountState } from "@crossbell/connect-kit";
 
-export function useMintNote(
-	characterId: number,
-	noteId: number,
-	toAddress: string
-) {
+export type UseMintNoteParams = {
+	characterId: number;
+	noteId: number;
+};
+
+export type UseMintNoteOptions = UseMutationOptions<
+	unknown,
+	unknown,
+	UseMintNoteParams
+>;
+
+export function useMintNote(options?: UseMintNoteOptions) {
+	const toAddress = useAccountState((s) => s.wallet?.address);
 	const contract = useContract();
 	const queryClient = useQueryClient();
 
 	return useMutation(
-		async () => {
-			return contract.mintNote(characterId, noteId, toAddress);
+		async ({ characterId, noteId }) => {
+			return contract.mintNote(characterId, noteId, toAddress!);
 		},
 		{
-			onSuccess: () => {
+			...options,
+
+			onSuccess: (...params) => {
+				const { characterId, noteId } = params[1];
+
 				return Promise.all([
+					options?.onSuccess?.(...params),
+
 					queryClient.invalidateQueries(
 						SCOPE_KEY_NOTE_STATUS(characterId, noteId)
 					),
@@ -29,14 +48,18 @@ export function useMintNote(
 						SCOPE_KEY_MINTED_NOTE_OF_NOTE(characterId, noteId)
 					),
 					queryClient.invalidateQueries(
-						SCOPE_KEY_MINTED_NOTE_OF_ADDRESS(toAddress)
+						SCOPE_KEY_MINTED_NOTE_OF_ADDRESS(toAddress!)
 					),
 				]);
 			},
-			onError: (err: any) => {
+			onError: (...params) => {
+				const err = params[0];
+
+				options?.onError?.(...params);
+
 				showNotification({
 					title: "Error while minting note",
-					message: err.message,
+					message: err instanceof Error ? err.message : `${err}`,
 					color: "red",
 				});
 			},
