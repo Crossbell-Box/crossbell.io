6ce73621624b8dcce83ef0855796f70bb9b3418c
Runjuu <hi@runjuu.com>
2023-01-11 22:31:06 -0800

    feat(connect-kit): `useUnlinkNote` add extra options

--- packages/connect-kit/src/hooks/link-note/index.ts
+++ packages/connect-kit/src/hooks/link-note/index.ts
@@ -1,12 +1,12 @@
 import { NoteLinkType } from "@crossbell/indexer";
 
 import { useLinkNote, UseLinkNoteOptions } from "./use-link-note";
-import { useUnlinkNote } from "./use-unlink-note";
+import { useUnlinkNote, UseUnlinkNoteOptions } from "./use-unlink-note";
 
 export function useLikeNote(options?: UseLinkNoteOptions) {
 	return useLinkNote(NoteLinkType.like, options);
 }
 
-export function useUnlikeNote() {
-	return useUnlinkNote(NoteLinkType.like);
+export function useUnlikeNote(options?: UseUnlinkNoteOptions) {
+	return useUnlinkNote(NoteLinkType.like, options);
 }
