f3757d7263a783b623f8edf6308a53a2e88e04ca
Runjuu <hi@runjuu.com>
2023-01-12 20:45:24 -0800

    feat(connect-kit): add extra options for follow/unfollow hooks

--- packages/connect-kit/src/hooks/link-character/index.ts
+++ packages/connect-kit/src/hooks/link-character/index.ts
@@ -10,13 +10,16 @@ import {
 import { useAccountState } from "../account-state";
 
 import { useLinkCharacter, LinkCharacterOptions } from "./use-link-character";
-import { useUnlinkCharacter } from "./use-unlink-character";
+import {
+	UnlinkCharacterOptions,
+	useUnlinkCharacter,
+} from "./use-unlink-character";
 import {
 	useLinkCharacters,
 	LinkCharactersOptions,
 } from "./use-link-characters";
 
-export function useFollowCharacter() {
+export function useFollowCharacter(_options?: LinkCharacterOptions) {
 	const queryClient = useQueryClient();
 	const currentCharacterId = useAccountState(
 		(s) => s.computed.account?.characterId
@@ -24,22 +27,29 @@ export function useFollowCharacter() {
 
 	const options: LinkCharacterOptions = React.useMemo(
 		() => ({
-			onSuccess: (_, { characterId }) => {
-				return Promise.all(
-					invalidateQueries(queryClient, {
+			..._options,
+
+			onSuccess: (...params) => {
+				const { characterId } = params[1];
+
+				return Promise.all([
+					_options?.onSuccess?.(...params),
+
+					...invalidateQueries({
+						queryClient,
 						characterId,
 						currentCharacterId,
-					})
-				);
+					}),
+				]);
 			},
 		}),
-		[queryClient, currentCharacterId]
+		[_options, queryClient, currentCharacterId]
 	);
 
 	return useLinkCharacter(CharacterLinkType.follow, options);
 }
 
-export function useFollowCharacters() {
+export function useFollowCharacters(_options?: LinkCharactersOptions) {
 	const queryClient = useQueryClient();
 	const currentCharacterId = useAccountState(
 		(s) => s.computed.account?.characterId
@@ -47,54 +57,69 @@ export function useFollowCharacters() {
 
 	const options: LinkCharactersOptions = React.useMemo(
 		() => ({
-			onSuccess: (_, { characterIds = [] }) =>
-				Promise.all(
-					characterIds.flatMap((characterId) =>
-						invalidateQueries(queryClient, {
+			..._options,
+
+			onSuccess: (...params) => {
+				const { characterIds = [] } = params[1];
+
+				return Promise.all([
+					_options?.onSuccess?.(...params),
+
+					...characterIds.flatMap((characterId) =>
+						invalidateQueries({
+							queryClient,
 							characterId,
 							currentCharacterId,
 						})
-					)
-				),
+					),
+				]);
+			},
 		}),
-		[queryClient, currentCharacterId]
+		[_options, queryClient, currentCharacterId]
 	);
 
 	return useLinkCharacters(CharacterLinkType.follow, options);
 }
 
-export function useUnfollowCharacter() {
+export function useUnfollowCharacter(_options?: UnlinkCharacterOptions) {
 	const queryClient = useQueryClient();
 	const currentCharacterId = useAccountState(
 		(s) => s.computed.account?.characterId
 	);
 
-	const options: LinkCharacterOptions = React.useMemo(
+	const options: UnlinkCharacterOptions = React.useMemo(
 		() => ({
-			onSuccess: (_, { characterId }) =>
-				Promise.all(
-					invalidateQueries(queryClient, {
+			..._options,
+
+			onSuccess: (...params) => {
+				const { characterId } = params[1];
+
+				return Promise.all([
+					_options?.onSuccess?.(...params),
+
+					...invalidateQueries({
+						queryClient,
 						characterId,
 						currentCharacterId,
-					})
-				),
+					}),
+				]);
+			},
 		}),
-		[queryClient, currentCharacterId]
+		[_options, queryClient, currentCharacterId]
 	);
 
 	return useUnlinkCharacter(CharacterLinkType.follow, options);
 }
 
-function invalidateQueries(
-	queryClient: ReturnType<typeof useQueryClient>,
-	{
-		characterId,
-		currentCharacterId,
-	}: {
-		currentCharacterId: number | undefined;
-		characterId: number;
-	}
-) {
+function invalidateQueries({
+	queryClient,
+	characterId,
+	currentCharacterId,
+}: {
+	queryClient: ReturnType<typeof useQueryClient>;
+	currentCharacterId: number | undefined;
+	characterId: number;
+}) {
 	return [
 		queryClient.invalidateQueries(
 			SCOPE_KEY_CHARACTER_FOLLOW_RELATION(currentCharacterId, characterId)
