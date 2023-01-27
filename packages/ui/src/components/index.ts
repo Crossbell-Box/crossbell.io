47bad927513516861cbbac5f2a7ad3df902e9db1
Runjuu <hi@runjuu.com>
2023-01-26 20:19:01 -0800

    feat(ui): create `toggle` component

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -2,3 +2,4 @@ export * from "./hooks-renderer";
 export * from "./character-avatar";
 export * from "./loading";
 export * from "./loading-overlay";
+export * from "./toggle";
---
+++ packages/ui/src/components/toggle/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import classNames from "classnames";
+import { useRefCallback } from "@crossbell/util-hooks";
+
+import styles from "./index.module.css";
+
+export type ToggleProps = {
+	isActive: boolean;
+	onToggle: (isActive: boolean) => void;
+	className?: string;
+};
+
+export function Toggle({ isActive, onToggle, className }: ToggleProps) {
+	const handleClick = useRefCallback((e: React.ChangeEvent<HTMLInputElement>) =>
+		onToggle(e.currentTarget.checked)
+	);
+
+	return (
+		<label className={classNames(styles.label, className)}>
+			<input
+				type="checkbox"
+				className={styles.input}
+				checked={isActive}
+				onChange={handleClick}
+			/>
+			<div className={styles.indicator} />
+		</label>
+	);
+}
