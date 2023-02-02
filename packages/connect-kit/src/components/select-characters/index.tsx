d2cfd85a3b161b6c400cfbff357c91e170f1f108
Runjuu <hi@runjuu.com>
2023-02-01 22:18:04 -0800

    feat(connect-kit/select-characters): add load more function

--- packages/connect-kit/src/components/select-characters/index.tsx
+++ packages/connect-kit/src/components/select-characters/index.tsx
@@ -7,16 +7,32 @@ import { List, ListProps } from "./list";
 import { Empty } from "./empty";
 import { BottomTips } from "../bottom-tips";
 
-export type SelectCharactersProps = Omit<ListProps, "characters">;
+export type SelectCharactersProps = Omit<
+	ListProps,
+	"characters" | "hasMore" | "onLoadMore" | "isLoading"
+>;
 
 export function SelectCharacters(props: SelectCharactersProps) {
-	const { characters, isLoading: isCharactersLoading } = useAccountCharacters();
+	const {
+		characters,
+		isLoading: isCharactersLoading,
+		isFetchingNextPage,
+		fetchNextPage,
+		hasNextPage,
+	} = useAccountCharacters();
 
 	return (
 		<>
 			<LoadingOverlay visible={isCharactersLoading} />
+
 			{characters.length > 0 ? (
-				<List characters={characters} {...props} />
+				<List
+					characters={characters}
+					onLoadMore={fetchNextPage}
+					hasMore={hasNextPage}
+					isLoading={isFetchingNextPage}
+					{...props}
+				/>
 			) : (
 				<Empty onSelectNew={props.onSelectNew} />
 			)}
--- packages/connect-kit/src/components/select-characters/list.tsx
+++ packages/connect-kit/src/components/select-characters/list.tsx
@@ -1,7 +1,13 @@
 import React from "react";
-import { CharacterAvatar, HooksRenderer, LoadingOverlay } from "@crossbell/ui";
+import {
+	CharacterAvatar,
+	HooksRenderer,
+	LoadingOverlay,
+	LoadMore,
+} from "@crossbell/ui";
 import { extractCharacterName } from "@crossbell/util-metadata";
 import { CharacterEntity } from "crossbell.js";
+import { ScrollArea } from "@mantine/core";
 
 import { useAccountState, useToggleOpSignOperator } from "../../hooks";
 
@@ -15,12 +21,18 @@ export type ListProps = {
 		character: CharacterEntity,
 		extraInfo: { opSignOperatorHasPermissions: boolean }
 	) => void;
+	onLoadMore: () => void;
+	hasMore: boolean;
+	isLoading: boolean;
 };
 
 export function List({
 	characters,
 	onSelectNew,
 	afterSelectCharacter,
+	onLoadMore,
+	hasMore,
+	isLoading,
 }: ListProps) {
 	const [currentCharacterId, switchCharacter] = useAccountState((s) => [
 		s.computed.account?.characterId,
@@ -28,64 +40,72 @@ export function List({
 	]);
 
 	return (
-		<OptionList className={styles.container}>
-			{characters.map((character) => (
-				<HooksRenderer
-					key={character.characterId}
-					hooks={useToggleOpSignOperator}
-					params={[character]}
-				>
-					{([{ hasPermissions }, { isLoading }]) => (
-						<>
-							<LoadingOverlay visible={isLoading} />
-							<OptionListItem
-								color={
-									currentCharacterId === character.characterId
-										? "green"
-										: "gray"
-								}
-								className={styles.characterItem}
-								title={character.handle}
-								onClick={async () => {
-									switchCharacter(character);
-									afterSelectCharacter(character, {
-										opSignOperatorHasPermissions: hasPermissions,
-									});
-								}}
-							>
-								<CharacterAvatar character={character} size={32} />
-								<div>
-									<div className={styles.characterName}>
-										{extractCharacterName(character)}
+		<ScrollArea.Autosize maxHeight="80vh">
+			<OptionList className={styles.container}>
+				{characters.map((character) => (
+					<HooksRenderer
+						key={character.characterId}
+						hooks={useToggleOpSignOperator}
+						params={[character]}
+					>
+						{([{ hasPermissions }, { isLoading }]) => (
+							<>
+								<LoadingOverlay visible={isLoading} />
+								<OptionListItem
+									color={
+										currentCharacterId === character.characterId
+											? "green"
+											: "gray"
+									}
+									className={styles.characterItem}
+									title={character.handle}
+									onClick={async () => {
+										switchCharacter(character);
+										afterSelectCharacter(character, {
+											opSignOperatorHasPermissions: hasPermissions,
+										});
+									}}
+								>
+									<CharacterAvatar character={character} size={32} />
+									<div>
+										<div className={styles.characterName}>
+											{extractCharacterName(character)}
+										</div>
+										<div className={styles.characterHandle}>
+											@{character.handle}
+										</div>
 									</div>
-									<div className={styles.characterHandle}>
-										@{character.handle}
-									</div>
-								</div>
-							</OptionListItem>
-						</>
-					)}
-				</HooksRenderer>
-			))}
+								</OptionListItem>
+							</>
+						)}
+					</HooksRenderer>
+				))}
 
-			<OptionListItem
-				color="gray"
-				className={styles.mintItem}
-				onClick={onSelectNew}
-			>
-				<svg
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
+				<LoadMore
+					onLoadMore={onLoadMore}
+					hasMore={hasMore}
+					isLoading={isLoading}
+				/>
+
+				<OptionListItem
+					color="gray"
+					className={styles.mintItem}
+					onClick={onSelectNew}
 				>
-					<path
-						d="M10.9778 24V13.0222H0V10.9778H10.9778V0H13.0222V10.9778H24V13.0222H13.0222V24H10.9778Z"
-						fill="#979DA4"
-					/>
-				</svg>
-			</OptionListItem>
-		</OptionList>
+					<svg
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							d="M10.9778 24V13.0222H0V10.9778H10.9778V0H13.0222V10.9778H24V13.0222H13.0222V24H10.9778Z"
+							fill="#979DA4"
+						/>
+					</svg>
+				</OptionListItem>
+			</OptionList>
+		</ScrollArea.Autosize>
 	);
 }
