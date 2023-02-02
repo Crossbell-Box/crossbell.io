9d58c7df53cca0553e4114aaaf9bd02d6f38d9f4
Runjuu <hi@runjuu.com>
2023-02-01 22:17:19 -0800

    feat(ui): create `load-more` component

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,6 +1,7 @@
 export * from "./hooks-renderer";
 export * from "./character-avatar";
 export * from "./dialog";
+export * from "./load-more";
 export * from "./loading";
 export * from "./loading-overlay";
 export * from "./toggle";
