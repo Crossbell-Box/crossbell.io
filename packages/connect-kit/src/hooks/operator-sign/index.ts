bb97c3e89e97ab4362a964fd85b6077640b0b9a7
Runjuu <hi@runjuu.com>
2023-02-09 11:06:55 -0800

    feat(connect-kit): create `useIsOpSignEnabled` hooks

--- packages/connect-kit/src/hooks/operator-sign/index.ts
+++ packages/connect-kit/src/hooks/operator-sign/index.ts
@@ -1,3 +1,4 @@
+export * from "./use-is-op-sign-enabled";
 export * from "./use-op-sign-balance";
 export * from "./use-toggle-op-sign-operator";
 export * from "./use-op-sign-operator-has-permissions";
