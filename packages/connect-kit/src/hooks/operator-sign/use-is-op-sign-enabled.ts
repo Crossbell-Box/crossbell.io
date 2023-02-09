b88e59236b93d1c2519453aed8ec99a2d0332547
Runjuu <hi@runjuu.com>
2023-02-09 11:16:40 -0800

    chore(connect-kit): rename `UseIsOpSignEnabled` to `UseIsOpSignEnabledConfig`

--- packages/connect-kit/src/hooks/operator-sign/use-is-op-sign-enabled.ts
+++ packages/connect-kit/src/hooks/operator-sign/use-is-op-sign-enabled.ts
@@ -1,11 +1,11 @@
 import { useIsWalletSignedIn } from "../use-is-wallet-signed-in";
 import { useOPSignOperatorHasPermissions } from "./use-op-sign-operator-has-permissions";
 
-export type UseIsOpSignEnabled = {
+export type UseIsOpSignEnabledConfig = {
 	characterId: number | null | undefined;
 };
 
-export function useIsOpSignEnabled({ characterId }: UseIsOpSignEnabled) {
+export function useIsOpSignEnabled({ characterId }: UseIsOpSignEnabledConfig) {
 	const isSignedIn = useIsWalletSignedIn();
 	const hasPermissions = useOPSignOperatorHasPermissions({ characterId });
 
