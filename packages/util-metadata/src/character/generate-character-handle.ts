7f716cad4d092fa6ecadf7008ea5d7a765929b0f
Runjuu <hi@runjuu.com>
2023-02-10 16:23:03 -0800

    feat(util-metadata/generateCharacterHandle): add default prefix

--- packages/util-metadata/src/character/generate-character-handle.ts
+++ packages/util-metadata/src/character/generate-character-handle.ts
@@ -1,12 +1,12 @@
 export function generateCharacterHandle(name: string) {
 	const randomNumber = Math.floor(Math.random() * 10000);
 	return (
-		name
+		(name
 			.trim()
 			.slice(0, 25)
 			.toLowerCase()
 			.replace(/\s/g, "-")
-			.replace(/[^a-z0-9-]/g, "") +
+			.replace(/[^a-z0-9-]/g, "") || "character") +
 		"-" +
 		randomNumber
 	);
