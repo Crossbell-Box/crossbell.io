c88080e7a31cb1f4d5aa8f4cf9ae03bfaa72a786
Runjuu <hi@runjuu.com>
2023-02-15 21:45:49 -0800

    feat(connect-kit): update `async-retry`
    support extra configs

--- packages/connect-kit/src/utils/async-retry.ts
+++ packages/connect-kit/src/utils/async-retry.ts
@@ -1,22 +1,45 @@
+import { MarkRequired } from "ts-essentials";
+
 const RETRY_SYMBOL = Symbol("async-retry:retry");
 
 export type AsyncRetryCallback<T> = (
 	RETRY: typeof RETRY_SYMBOL
 ) => Promise<T | typeof RETRY_SYMBOL>;
 
+export type AsyncRetryConfig<T> = {
+	delayMs?: number;
+	maxRetryTimes?: number;
+	defaultValue?: T;
+};
+
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export async function asyncRetry<T>(
 	callback: AsyncRetryCallback<T>,
-	delayMs = 1000
-): Promise<T> {
+	config: MarkRequired<AsyncRetryConfig<T>, "defaultValue">
+): Promise<T>;
+export async function asyncRetry<T>(
+	callback: AsyncRetryCallback<T>,
+	config?: AsyncRetryConfig<T>
+): Promise<T | undefined>;
+export async function asyncRetry<T>(
+	callback: AsyncRetryCallback<T>,
+	{ delayMs = 1000, maxRetryTimes = 5, defaultValue }: AsyncRetryConfig<T> = {}
+): Promise<T | undefined> {
+	let retryTimes = 0;
+
 	while (true) {
 		const result = await callback(RETRY_SYMBOL);
 
 		if (result !== RETRY_SYMBOL) {
 			return result;
 		} else {
-			await sleep(delayMs);
+			if (retryTimes < maxRetryTimes) {
+				await sleep(delayMs);
+				retryTimes += 1;
+			} else {
+				return defaultValue;
+			}
 		}
 	}
 }
