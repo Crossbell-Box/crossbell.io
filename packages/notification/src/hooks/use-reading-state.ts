d1417deed1c486288bca6242c6492533982bf457
Runjuu <hi@runjuu.com>
2023-02-17 23:02:51 -0800

    deps: update `zustand`

--- packages/notification/src/hooks/use-reading-state.ts
+++ packages/notification/src/hooks/use-reading-state.ts
@@ -1,4 +1,4 @@
-import create from "zustand";
+import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
 import { persist } from "zustand/middleware";
 import { ParsedNotification } from "@crossbell/indexer";
