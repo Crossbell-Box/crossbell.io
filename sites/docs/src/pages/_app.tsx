c4a8c59b5a6e0a5019c0f53bf9cc4fd6555c6c38
Runjuu <hi@runjuu.com>
2023-03-01 11:29:51 -0800

    docs: init `@crossbell/connect-kit`

--- sites/docs/src/pages/_app.tsx
+++ sites/docs/src/pages/_app.tsx
@@ -1,7 +1,18 @@
 import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
+import { WagmiConfig } from "wagmi";
+
+import { ConnectKitProvider, getDefaultClient } from "@crossbell/connect-kit";
+
+const client = getDefaultClient({ appName: "Crossbell Dev" });
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<WagmiConfig client={client}>
+			<ConnectKitProvider>
+				<Component {...pageProps} />
+			</ConnectKitProvider>
+		</WagmiConfig>
+	);
 }
