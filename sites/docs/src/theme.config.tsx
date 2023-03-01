b3cbaa38931f91ecfe5577155f34f45a78c65807
Runjuu <hi@runjuu.com>
2023-03-01 10:48:40 -0800

    docs: update theme config

--- sites/docs/src/theme.config.tsx
+++ sites/docs/src/theme.config.tsx
@@ -1,8 +1,35 @@
+import { DocsThemeConfig } from "nextra-theme-docs";
 import { CrossbellLogo } from "@crossbell/ui";
+import { useRouter } from "next/router";
 
-export default {
-	logo: <CrossbellLogo />,
+const config: DocsThemeConfig = {
+	logo: (
+		<span className="flex items-center gap-2">
+			<CrossbellLogo />
+			<span className="text-xs font-black -translate-y-1/2">DEV</span>
+		</span>
+	),
+	docsRepositoryBase:
+		"https://github.com/Crossbell-Box/crossbell-universe/tree/main/sites/docs",
 	project: {
 		link: "https://github.com/Crossbell-Box/crossbell-universe",
 	},
+	useNextSeoProps() {
+		const { route } = useRouter();
+
+		return {
+			titleTemplate: route !== "/" ? "%s – Crossbell Dev" : "%s",
+		};
+	},
+	footer: {
+		text() {
+			return (
+				<div>
+					<p>MIT © 2023 Natural Selection Labs</p>
+				</div>
+			);
+		},
+	},
 };
+
+export default config;
