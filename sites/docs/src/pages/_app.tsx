a10206495aa180171f73d6fceb6392d1255e0ab4
Runjuu <hi@runjuu.com>
2023-03-01 12:32:26 -0800

    docs: create `try-it-out` page

--- sites/docs/src/pages/_app.tsx
+++ sites/docs/src/pages/_app.tsx
@@ -2,17 +2,20 @@ import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
 import { WagmiConfig } from "wagmi";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConnectKitProvider, getDefaultClient } from "@crossbell/connect-kit";
 
-const client = getDefaultClient({ appName: "Crossbell Dev" });
+const wagmiClient = getDefaultClient({ appName: "Crossbell Dev" });
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<WagmiConfig client={client}>
-			<ConnectKitProvider>
-				<Component {...pageProps} />
-			</ConnectKitProvider>
-		</WagmiConfig>
+		<QueryClientProvider client={queryClient}>
+			<WagmiConfig client={wagmiClient}>
+				<ConnectKitProvider>
+					<Component {...pageProps} />
+				</ConnectKitProvider>
+			</WagmiConfig>
+		</QueryClientProvider>
 	);
 }
