539f9629c44f2653647060812b6828e086fb2175
Runjuu <hi@runjuu.com>
2023-03-01 11:36:40 -0800

    docs: display current version

--- sites/docs/src/theme.config.tsx
+++ sites/docs/src/theme.config.tsx
@@ -1,12 +1,16 @@
 import { DocsThemeConfig } from "nextra-theme-docs";
 import { CrossbellLogo } from "@crossbell/ui";
 import { useRouter } from "next/router";
+import { version } from "../package.json";
 
 const config: DocsThemeConfig = {
 	logo: (
 		<span className="flex items-center gap-2">
 			<CrossbellLogo />
-			<span className="text-xs font-black -translate-y-1/2">DEV</span>
+			<div className="flex flex-col text-xs">
+				<span className="font-black">DEV</span>
+				<span className="opacity-60 font-mono">{version}</span>
+			</div>
 		</span>
 	),
 	docsRepositoryBase:
