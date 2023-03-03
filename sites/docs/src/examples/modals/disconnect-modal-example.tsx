a40f42b0adfd9c7b81244201bce7f7f58d831f80
Runjuu <hi@runjuu.com>
2023-03-02 16:47:09 -0800

    docs: update `Connect Modal` & `Disconnect Modal`

--- sites/docs/src/examples/modals/disconnect-modal-example.tsx
+++ sites/docs/src/examples/modals/disconnect-modal-example.tsx
@@ -2,27 +2,26 @@ import React from "react";
 import {
 	useConnectModal,
 	useDisconnectModal,
-	useAccountState,
+	useIsConnected,
+	useIsSsrReady,
 } from "@crossbell/connect-kit";
 import classNames from "classnames";
 
 export function DisconnectModalExample() {
 	const connectModal = useConnectModal();
 	const disconnectModal = useDisconnectModal();
-	const [account, ssrReady] = useAccountState((s) => [
-		s.computed.account,
-		s.ssrReady,
-	]);
+	const isConnected = useIsConnected();
+	const isSsrReady = useIsSsrReady();
 
 	return (
 		<div className="bg-slate-500/10 rounded-2xl">
 			<div
 				className={classNames(
 					"transition flex flex-col items-center justify-center gap-5 h-60",
-					ssrReady ? "opacity-100" : "opacity-0"
+					isSsrReady ? "opacity-100" : "opacity-0"
 				)}
 			>
-				{account ? (
+				{isConnected ? (
 					<button className="btn btn-danger" onClick={disconnectModal.show}>
 						Disconnect
 					</button>
