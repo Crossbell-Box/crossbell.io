917ad0fcbe5b06fe762c97f116d5350303a812d2
Runjuu <hi@runjuu.com>
2023-03-02 17:08:27 -0800

    feat(connect-kit): deprecate `useUpgradeAccountModal`
    use `useUpgradeEmailAccountModal` instead

--- packages/connect-kit/src/modals/upgrade-account-modal/index.tsx
+++ packages/connect-kit/src/modals/upgrade-account-modal/index.tsx
@@ -2,9 +2,20 @@ import { SelectOptions } from "../../scenes/upgrade-account";
 import { createDynamicScenesModal } from "../../components";
 
 export const {
+	/**
+	 * @deprecated use `useUpgradeEmailAccountModal` instead.
+	 */
 	useModal: useUpgradeAccountModal,
+	/**
+	 * @deprecated use `showUpgradeEmailAccountModal` instead.
+	 */
 	showModal: showUpgradeAccountModal,
 } = createDynamicScenesModal("upgrade-account", {
 	kind: "select-upgrade-account-options",
 	Component: SelectOptions,
 });
+
+export {
+	useUpgradeAccountModal as useUpgradeEmailAccountModal,
+	showUpgradeAccountModal as showUpgradeEmailAccountModal,
+};
