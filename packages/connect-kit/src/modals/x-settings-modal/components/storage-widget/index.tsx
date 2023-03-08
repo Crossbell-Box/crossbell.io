8b7083bb69d644f5a086bdd3bf2eee2d453ccf7b
Runjuu <hi@runjuu.com>
2023-03-08 15:52:27 -0800

    feat(connect-kit/x-settings): add `beta` tag to storage widget

--- packages/connect-kit/src/modals/x-settings-modal/components/storage-widget/index.tsx
+++ packages/connect-kit/src/modals/x-settings-modal/components/storage-widget/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import prettyBytes from "pretty-bytes";
 import { useCharacterMediaUsage } from "@crossbell/connect-kit";
+import { Tooltip } from "@mantine/core";
 
 import styles from "./index.module.css";
 
@@ -15,6 +16,22 @@ export function StorageWidget({ characterId }: StorageWidgetProps) {
 		<div className={styles.container}>
 			<div className={styles.info}>
 				<span className={styles.title}>Unlimited Storage</span>
+
+				<Tooltip
+					multiline
+					width={223}
+					label="Our storage is in beta, providing unlimited storage. When officially launched, storage may be limited and payment required."
+					style={{
+						background: "rgba(94, 100, 115, 0.7)",
+						backdropFilter: "blur(5px)",
+						borderRadius: "6px",
+						padding: "8px 16px",
+						fontSize: "12px",
+						fontWeight: 400,
+					}}
+				>
+					<span className={styles.beta}>Beta</span>
+				</Tooltip>
 
 				<span className={styles.amount}>
 					{prettyBytes(mediaUsage ?? 0, { maximumFractionDigits: 2 })}/
