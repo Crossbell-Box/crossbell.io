22a823c486323e18fb07ec78c25cf8eb7b710559
Runjuu <hi@runjuu.com>
2023-03-08 17:55:02 -0800

    feat(ui/icons): add `settings-switch-app` & `x-log-logo`

--- packages/ui/svgr-index-template.js
+++ packages/ui/svgr-index-template.js
@@ -4,7 +4,9 @@ const { pascalCase } = require("pascal-case");
 function defaultIndexTemplate(filePaths) {
 	const exportEntries = filePaths.map((filePath) => {
 		const basename = path.basename(filePath, path.extname(filePath));
-		const exportName = `${pascalCase(basename)}Icon`;
+		const exportName = /^.+logo$/i.test(basename)
+			? pascalCase(basename)
+			: `${pascalCase(basename)}Icon`;
 		return `export { default as ${exportName} } from './${basename}'`;
 	});
 	return exportEntries.join("\n");
