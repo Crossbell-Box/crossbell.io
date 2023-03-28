066c9bf30fc8373237d3835fa6c8cf1ed426b1ea
Runjuu <hi@runjuu.com>
2023-03-27 18:08:48 -0700

    feat(ui/icons): add `medium` & `substack`

---
+++ packages/ui/src/icons/svgr/substack.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { SVGProps, Ref, forwardRef } from "react";
+const SvgSubstack = (
+	props: SVGProps<SVGSVGElement>,
+	ref: Ref<SVGSVGElement>
+) => (
+	<svg
+		width="1em"
+		height="1em"
+		viewBox="0 0 12 12"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+		ref={ref}
+		{...props}
+	>
+		<path
+			d="M9.526 4.747H2.5v-.945h7.027v.945ZM2.5 5.604V10l3.513-1.963L9.527 10V5.604H2.5ZM9.527 2H2.5v.945h7.027V2Z"
+			fill="currentColor"
+		/>
+	</svg>
+);
+const ForwardRef = forwardRef(SvgSubstack);
+export default ForwardRef;
