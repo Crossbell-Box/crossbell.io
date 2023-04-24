10c2b245cfd989ef144cfaa015da96c8394dd2ab
Runjuu <hi@runjuu.com>
2023-04-24 10:02:45 -0700

    chore: update deps

--- packages/indexer/src/apis/csb.ts
+++ packages/indexer/src/apis/csb.ts
@@ -1,5 +1,5 @@
 import { BigNumber } from "ethers";
-import LRUCache from "lru-cache";
+import { LRUCache } from "lru-cache";
 
 const cache = new LRUCache({ ttl: 1000 * 10, max: 10 }); // 10s cache
 
