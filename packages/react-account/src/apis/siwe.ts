46235a092d712331a8f6d8f89865b3a059fffbb9
Runjuu <hi@runjuu.com>
2023-04-26 12:55:53 -0700

    feat(react-account/apis): `siweSignIn` add fallback uri

--- packages/react-account/src/apis/siwe.ts
+++ packages/react-account/src/apis/siwe.ts
@@ -17,8 +17,8 @@ export async function siweSignIn(signer: Signer): Promise<Siwe> {
 		method: "POST",
 		body: {
 			address,
-			domain: window.location.host,
-			uri: window.location.origin,
+			domain: window.location.host ?? "crossbell.io",
+			uri: window.location.origin ?? "https://crossbell.io",
 			statement: "Sign in with Crossbell to the app.",
 		},
 	});
