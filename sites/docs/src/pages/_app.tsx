2b1c5f537394301cc2ab98f4d5eace835ebac1e4
Runjuu <hi@runjuu.com>
2023-05-17 15:46:45 -0700

    docs: update `quick-start.mdx`

--- sites/docs/src/pages/_app.tsx
+++ sites/docs/src/pages/_app.tsx
@@ -12,7 +12,7 @@ import {
 } from "@crossbell/connect-kit";
 import { NotificationModal } from "@crossbell/notification";
 
-const wagmiClient = createWagmiConfig({ appName: "Crossbell Dev" });
+const wagmiConfig = createWagmiConfig({ appName: "Crossbell Dev" });
 const queryClient = new QueryClient();
 const dsn =
 	"https://8e72f50cf3a04612ac5c544dabc3bcab@o4504811532517376.ingest.sentry.io/4504840965259264";
@@ -38,7 +38,7 @@ const xSettings: Partial<XSettingsConfig> = {
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<QueryClientProvider client={queryClient}>
-			<WagmiConfig config={wagmiClient}>
+			<WagmiConfig config={wagmiConfig}>
 				<ConnectKitProvider
 					xSettings={xSettings}
 					ignoreWalletDisconnectEvent={true}
