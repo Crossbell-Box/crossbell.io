215ec85111e5536156a787a0a07a702db5fba0b5
Runjuu <hi@runjuu.com>
2023-06-16 11:33:29 -0700

    feat(connect-kit): preload images only when the page is idle

--- packages/util-hooks/src/use-preload-imgs.ts
+++ packages/util-hooks/src/use-preload-imgs.ts
@@ -2,9 +2,15 @@ import React from "react";
 
 export function usePreloadImgs(imgs: string[]) {
 	React.useEffect(() => {
-		imgs.forEach((img) => {
-			const image = new Image();
-			image.src = img;
-		});
+		if ("requestIdleCallback" in window) {
+			const handle = requestIdleCallback(() => {
+				imgs.forEach((img) => {
+					const image = new Image();
+					image.src = img;
+				});
+			});
+
+			return () => cancelIdleCallback(handle);
+		}
 	}, [imgs]);
 }
