b8cf56856f3330ea18b3ca58a001b3cf624353a9
Runjuu <hi@runjuu.com>
2023-06-21 15:54:06 -0700

    feat(connect-kit): lazy load `csb-detail-modal`

--- packages/connect-kit/src/modals/csb-detail-modal/index.tsx
+++ packages/connect-kit/src/modals/csb-detail-modal/index.tsx
@@ -1,65 +1,12 @@
 import React from "react";
-import { DynamicContainer, DynamicContainerContent } from "@crossbell/ui";
 
-import { BaseModal, Congrats } from "../../components";
+import { createLazyModal } from "../../components";
 
-import { SceneKind } from "./types";
-import { useScenesStore, useCsbDetailModal, StoresProvider } from "./stores";
-
-import { Balance } from "./scenes/balance";
-import { ClaimCSB } from "./scenes/claim-csb";
-import { Transfer } from "./scenes/transfer";
-import { TransferSuccess } from "./scenes/transfer-success";
+import { useCsbDetailModal } from "./stores";
 
 export { useCsbDetailModal };
 
-export function CsbDetailModal() {
-	const { isActive, hide } = useCsbDetailModal();
-	const storeKey = useResetStore();
-
-	return (
-		<BaseModal isActive={isActive} onClose={hide}>
-			<DynamicContainer>
-				<StoresProvider key={storeKey}>
-					<Main />
-				</StoresProvider>
-			</DynamicContainer>
-		</BaseModal>
-	);
-}
-
-function Main() {
-	const currentScene = useScenesStore(({ computed }) => computed.currentScene);
-
-	return (
-		<DynamicContainerContent id={currentScene.kind}>
-			{((): JSX.Element => {
-				switch (currentScene.kind) {
-					case SceneKind.balance:
-						return <Balance />;
-					case SceneKind.claimCSB:
-						return <ClaimCSB />;
-					case SceneKind.congrats:
-						return <Congrats {...currentScene} />;
-					case SceneKind.transfer:
-						return <Transfer />;
-					case SceneKind.transferSuccess:
-						return <TransferSuccess />;
-				}
-			})()}
-		</DynamicContainerContent>
-	);
-}
-
-function useResetStore() {
-	const { isActive } = useCsbDetailModal();
-	const keyRef = React.useRef(0);
-
-	React.useMemo(() => {
-		if (isActive) {
-			keyRef.current += 1;
-		}
-	}, [isActive]);
-
-	return keyRef.current;
-}
+export const CsbDetailModal = createLazyModal(
+	useCsbDetailModal,
+	React.lazy(() => import("./lazy"))
+);
