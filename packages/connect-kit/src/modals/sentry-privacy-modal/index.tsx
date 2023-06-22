595193e457089540d3a8a42f13727c859502fada
Runjuu <hi@runjuu.com>
2023-06-22 15:06:07 -0700

    feat(connect-kit): lazy load `sentry-privacy-modal`

--- packages/connect-kit/src/modals/sentry-privacy-modal/index.tsx
+++ packages/connect-kit/src/modals/sentry-privacy-modal/index.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import { Dialog } from "@crossbell/ui";
 
 import { useSentryPrivacyModal } from "./stores";
-import { Privacy } from "./privacy";
+import { createLazyModal } from "../../components";
+import styles from "./privacy.module.css";
 
 export { useSentryPrivacyModal };
 
-export function SentryPrivacyModal() {
-	const { isActive } = useSentryPrivacyModal();
-
-	return (
-		<Dialog isActive={isActive}>
-			<Privacy />
-		</Dialog>
-	);
-}
+export const SentryPrivacyModal = createLazyModal(
+	useSentryPrivacyModal,
+	React.lazy(() => import("./privacy")),
+	{
+		Modal: Dialog,
+		loadingClassName: styles.container,
+	}
+);
--- packages/connect-kit/src/modals/sentry-privacy-modal/privacy.tsx
+++ packages/connect-kit/src/modals/sentry-privacy-modal/privacy.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { LoadingOverlay } from "@crossbell/ui";
+import { LoadingOverlay, DynamicContainerContent } from "@crossbell/ui";
 
 import { ModalHeader, OptionList, OptionListItem } from "../../components";
 import { useAppName, useSentryStatus } from "../../hooks";
@@ -7,41 +7,43 @@ import { useAppName, useSentryStatus } from "../../hooks";
 import { useSentryPrivacyModal } from "./stores";
 import styles from "./privacy.module.css";
 
-export function Privacy() {
+export default function Privacy() {
 	const { hide } = useSentryPrivacyModal();
 	const { enable, isEnabled, isLoading } = useSentryStatus({ onSuccess: hide });
 	const appName = useAppName();
 
 	return (
-		<div className={styles.container}>
-			<LoadingOverlay visible={isLoading} />
-			<ModalHeader title="Your Privacy" onClose={hide} />
+		<DynamicContainerContent id="privacy">
+			<div className={styles.container}>
+				<LoadingOverlay visible={isLoading} />
+				<ModalHeader title="Your Privacy" onClose={hide} />
 
-			<div className={styles.main}>
-				<p className={styles.tips}>
-					By clicking "Accept Sentry Monitoring", you agree that
-					{` ${appName} `}
-					can use Sentry to collect necessary information to improve your
-					experience in accordance with Sentry's [
-					<a href="https://sentry.io/legal/dpa/" target="_blank">
-						Data Processing Addendum
-					</a>
-					].
-				</p>
+				<div className={styles.main}>
+					<p className={styles.tips}>
+						By clicking "Accept Sentry Monitoring", you agree that
+						{` ${appName} `}
+						can use Sentry to collect necessary information to improve your
+						experience in accordance with Sentry's [
+						<a href="https://sentry.io/legal/dpa/" target="_blank">
+							Data Processing Addendum
+						</a>
+						].
+					</p>
 
-				<OptionList>
-					<OptionListItem
-						color="yellow"
-						className={styles.item}
-						onClick={isEnabled ? hide : enable}
-					>
-						Accept Sentry Monitoring
-					</OptionListItem>
-					<OptionListItem color="gray" className={styles.item} onClick={hide}>
-						Skip
-					</OptionListItem>
-				</OptionList>
+					<OptionList>
+						<OptionListItem
+							color="yellow"
+							className={styles.item}
+							onClick={isEnabled ? hide : enable}
+						>
+							Accept Sentry Monitoring
+						</OptionListItem>
+						<OptionListItem color="gray" className={styles.item} onClick={hide}>
+							Skip
+						</OptionListItem>
+					</OptionList>
+				</div>
 			</div>
-		</div>
+		</DynamicContainerContent>
 	);
 }
