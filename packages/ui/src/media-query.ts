cac044a38b20cfde44b0122181346f51072661ae
Runjuu <hi@runjuu.com>
2023-06-26 15:05:17 -0700

    chore: update deps

--- packages/ui/src/media-query.ts
+++ packages/ui/src/media-query.ts
@@ -1,3 +1,8 @@
-import { breakpoints } from "~/scripts/unocss/breakpoints";
-
-export { breakpoints };
+export const breakpoints = {
+	// https://mantine.dev/styles/responsive/#configure-breakpoints
+	xs: 576,
+	sm: 768,
+	md: 992,
+	lg: 1200,
+	xl: 1440,
+};
--- scripts/unocss/breakpoints.ts
+++
@@ -1,8 +0,0 @@
-export const breakpoints = {
-	// https://mantine.dev/styles/responsive/#configure-breakpoints
-	xs: 576,
-	sm: 768,
-	md: 992,
-	lg: 1200,
-	xl: 1440,
-};
