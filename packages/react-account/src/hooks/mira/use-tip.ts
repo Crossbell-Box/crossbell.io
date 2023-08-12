68da84a2fbeba05dd8bac0f8f169dfd328f187d9
Runjuu <hi@runjuu.com>
2023-08-12 12:27:42 -0700

    feat(react-account/use-tip): support `tipsWithFee` contract

--- packages/react-account/src/hooks/mira/use-tip.ts
+++ packages/react-account/src/hooks/mira/use-tip.ts
@@ -1,4 +1,5 @@
 import type { Numberish } from "crossbell";
+import type { Address } from "viem";
 
 import { getMiraTokenDecimals, emailTip } from "../../apis";
 import { createAccountTypeBasedMutationHooks } from "../account-type-based-hooks";
@@ -6,61 +7,78 @@ import { createAccountTypeBasedMutationHooks } from "../account-type-based-hooks
 import { SCOPE_KEY_TIPS_LIST } from "./use-tip-list";
 import { SCOPE_KEY_ACCOUNT_MIRA_BALANCE } from "./use-account-mira-balance";
 
-export const useTip = createAccountTypeBasedMutationHooks<
-	void,
-	{ characterId: Numberish; noteId?: Numberish; amount: number }
->({ actionDesc: "send tip", withParams: false }, () => ({
-	wallet: {
-		supportOPSign: false,
+export type UseTipOptions = {
+	amount: number;
+	characterId: Numberish;
+	noteId?: Numberish;
+	feeReceiver?: Address;
+};
 
-		async action({ characterId, noteId, amount }, { contract, account }) {
-			if (account.characterId) {
-				const decimal = await getMiraTokenDecimals(contract);
+export const useTip = createAccountTypeBasedMutationHooks<void, UseTipOptions>(
+	{ actionDesc: "send tip", withParams: false },
+	() => ({
+		wallet: {
+			supportOPSign: false,
 
-				if (noteId) {
-					return contract?.tips.tipCharacterForNote({
+			async action(
+				{ characterId, noteId: toNoteId, amount, feeReceiver },
+				{ contract, account },
+			) {
+				if (account.characterId) {
+					const decimal = await getMiraTokenDecimals(contract);
+					const options = {
 						fromCharacterId: account.characterId,
+						toCharacterId: characterId,
+						amount: BigInt(amount) * BigInt(10) ** BigInt(decimal),
+					};
+
+					if (toNoteId) {
+						return feeReceiver
+							? contract.tipsWithFee.tipCharacterForNote({
+									...options,
+									toNoteId,
+									feeReceiver,
+							  })
+							: contract.tips.tipCharacterForNote({
+									...options,
+									toNoteId,
+							  });
+					} else {
+						return feeReceiver
+							? contract.tipsWithFee.tipCharacter({ ...options, feeReceiver })
+							: contract.tips.tipCharacter(options);
+					}
+				}
+			},
+		},
+
+		async email({ characterId, noteId, amount }, { contract, account }) {
+			const decimal = await getMiraTokenDecimals(contract);
+
+			return emailTip({
+				token: account.token,
+				characterId,
+				noteId,
+				amount: BigInt(amount) * BigInt(10) ** BigInt(decimal),
+			});
+		},
+
+		onSuccess({ queryClient, variables, account }) {
+			const { characterId, noteId } = variables;
+
+			return Promise.all([
+				queryClient.invalidateQueries(
+					SCOPE_KEY_ACCOUNT_MIRA_BALANCE({ address: account!.address! }),
+				),
+
+				queryClient.invalidateQueries(
+					SCOPE_KEY_TIPS_LIST({
 						toCharacterId: characterId,
 						toNoteId: noteId,
-						amount: BigInt(amount) * BigInt(10) ** BigInt(decimal),
-					});
-				} else {
-					return contract?.tips.tipCharacter({
-						fromCharacterId: account.characterId,
-						toCharacterId: characterId,
-						amount: BigInt(amount) * BigInt(10) ** BigInt(decimal),
-					});
-				}
-			}
+						characterId: account?.characterId,
+					}),
+				),
+			]);
 		},
-	},
-
-	async email({ characterId, noteId, amount }, { contract, account }) {
-		const decimal = await getMiraTokenDecimals(contract);
-
-		return emailTip({
-			token: account.token,
-			characterId,
-			noteId,
-			amount: BigInt(amount) * BigInt(10) ** BigInt(decimal),
-		});
-	},
-
-	onSuccess({ queryClient, variables, account }) {
-		const { characterId, noteId } = variables;
-
-		return Promise.all([
-			queryClient.invalidateQueries(
-				SCOPE_KEY_ACCOUNT_MIRA_BALANCE({ address: account!.address! }),
-			),
-
-			queryClient.invalidateQueries(
-				SCOPE_KEY_TIPS_LIST({
-					toCharacterId: characterId,
-					toNoteId: noteId,
-					characterId: account?.characterId,
-				}),
-			),
-		]);
-	},
-}));
+	}),
+);
