2d438f89da7e09c2d80d13f8f98c43438ac2abb2
Runjuu <hi@runjuu.com>
2023-08-21 11:42:52 -0700

    refactor(react-account): use-mint-character
    to use createAccountTypeBasedMutationHooks

--- packages/react-account/src/hooks/mint-character/use-mint-character.ts
+++ packages/react-account/src/hooks/mint-character/use-mint-character.ts
@@ -1,9 +1,7 @@
 import { useUploadToIpfs } from "@crossbell/util-hooks";
-import { useMutation, UseMutationOptions } from "@tanstack/react-query";
 
-import { useHandleError } from "../use-handle-error";
+import { createAccountTypeBasedMutationHooks } from "../account-type-based-hooks";
 import { useCreateCharacter } from "../use-create-character";
-
 import { BaseCharacterProfileForm } from "./use-character-profile-form";
 
 export type UseMintCharacterParams = Pick<
@@ -11,43 +9,41 @@ export type UseMintCharacterParams = Pick<
 	"avatar" | "handle" | "username" | "bio"
 >;
 
-export type UseMintCharacterParamsOptions = UseMutationOptions<
-	unknown,
-	unknown,
+export const useMintCharacter = createAccountTypeBasedMutationHooks<
+	void,
 	UseMintCharacterParams
->;
+>(
+	{
+		actionDesc: "mint-character",
+		withParams: false,
+		connectType: "wallet",
+	},
+	() => {
+		const createCharacter = useCreateCharacter();
+		const uploadToIpfs = useUploadToIpfs();
 
-export function useMintCharacter(options?: UseMintCharacterParamsOptions) {
-	const createCharacter = useCreateCharacter();
-	const uploadToIpfs = useUploadToIpfs();
-	const handleError = useHandleError("Mint Character");
+		return {
+			wallet: {
+				supportOPSign: false,
+				async action({ bio, avatar: file, handle, username }) {
+					if (!handle || !username) return;
 
-	return useMutation(
-		async ({ bio, avatar: file, handle, username }) => {
-			if (!handle || !username) return;
+					const avatar = file
+						? typeof file === "string"
+							? file
+							: await uploadToIpfs.mutateAsync(file)
+						: null;
 
-			const avatar = file
-				? typeof file === "string"
-					? file
-					: await uploadToIpfs.mutateAsync(file)
-				: null;
-
-			await createCharacter.mutateAsync({
-				handle,
-				metadata: {
-					bio,
-					name: username,
-					avatars: [avatar].filter(Boolean) as string[],
+					await createCharacter.mutateAsync({
+						handle,
+						metadata: {
+							bio,
+							name: username,
+							avatars: [avatar].filter(Boolean) as string[],
+						},
+					});
 				},
-			});
-		},
-		{
-			...options,
-
-			onError(...params) {
-				options?.onError?.(...params);
-				handleError(params[0]);
 			},
-		},
-	);
-}
+		};
+	},
+);
